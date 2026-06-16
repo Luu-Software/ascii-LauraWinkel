db869e3dd4a337a7284fa065cd90f808c3a97d4f
LauraWinkel <laurawinkel10@gmail.com>
2026-06-16 16:01:07 -0300

    empeze la consigna base

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { preguntar } from './lib/consola.ts';
 
+let tam = tamaño
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+  // Para armar la fila de caracteres, construye un fila
+  letra = caracter
+  numero = tamaño 
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+// Imprimir las filas
+
+while (caracter )
